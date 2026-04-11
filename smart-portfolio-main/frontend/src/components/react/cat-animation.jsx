eb4c3ecec14b98465cf4582864890735b3d0993e
morningstar004 <pranjalkumar000007@gmail.com>
2026-04-11 16:19:20 +0530

    Added footer css and a cat animation

---
+++ smart-portfolio-main/frontend/src/components/react/cat-animation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const Cat = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/e79bd7f4-2707-4289-a132-abbea0c9be3c/M5yDKn6r4l.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+
+export default Cat;
